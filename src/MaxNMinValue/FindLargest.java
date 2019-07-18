b5e30b0205d7411d58cb829c42e24d61b32a0dee
Neeraj <neer.chaudhary@gmail.com>
2019-07-18 23:29:38 +0530

    smallast and larget program
    Signed-off-by: Neeraj <neer.chaudhary@gmail.com>

---
+++ src/MaxNMinValue/FindLargest.java
@@ -0,0 +1,23 @@
+package MaxNMinValue;
+
+public class FindLargest {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 75, 12, 15, 20, 45, 5, 71, 9};
+
+        int len = arr.length;
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 0; i < len; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            } else if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println( "minimum value is: " + min );
+        System.out.println( "maximum value is: " + max );
+    }
+}
