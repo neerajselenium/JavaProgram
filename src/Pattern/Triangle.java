3beeef2634e2af3027886c145d7c76453462ba7b
Neeraj <neer.chaudhary@gmail.com>
2019-07-23 23:02:18 +0530

    added .gitignore file
    Signed-off-by: Neeraj <neer.chaudhary@gmail.com>

---
+++ src/Pattern/Triangle.java
@@ -0,0 +1,24 @@
+package Pattern;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+
+        int num = 7;
+
+        for (int i = num - 1; i > 0; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print( " " );
+            }
+
+            for (int j = i; j <= num; j++) {
+                System.out.print( "*" + " " );
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
